5c290a9c09fd6d9f6b3799924e38b019b2c285b6
İpek <84208812+ipekguler@users.noreply.github.com>
2021-07-04 03:29:37 +0300

    Add files via upload

---
+++ SQL_HW3.sql
@@ -0,0 +1,14 @@
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+SELECT country FROM country
+WHERE length(country)>=6
+AND country LIKE '%n';
+
+SELECT title FROM film
+WHERE (LENGTH(title) - LENGTH(REPLACE(title, 't', ''))) + (LENGTH(title) - LENGTH(REPLACE(title, 'T', '')))>=4;
+
+SELECT * FROM film
+WHERE title LIKE 'C%'
+AND length>90
+AND rental_rate=2.99;
